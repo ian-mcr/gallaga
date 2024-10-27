be5f2b5f6d9c3034ace4e22f1fad42a1eb17abc2
ian-mcr <115840092+ian-mcr@users.noreply.github.com>
2024-10-27 17:02:59 +0000

    Add files via upload

--- gallaga.py
+++ gallaga.py
@@ -1,7 +1,9 @@
 import pgzrun
+import random
 WIDTH=800
 HEIGHT=600
 TITLE="Gallaga"
+ammo=[]
 coolship=Actor("coolship")
 coolship.pos=(400,550)
 alienbug=Actor("alienbug")
@@ -10,11 +12,36 @@ def draw():
     screen.blit("space",(0,0))
     coolship.draw()
     alienbug.draw()
+    for bullet in ammo:
+        bullet.draw()
 
 
 
 def update():
+    
     alienbug.y=alienbug.y+3
+    for bullet in ammo:
+        bullet.y=bullet.y-3
+    if alienbug.y>HEIGHT:
+        alienbug.pos=(random.randint(50,WIDTH),0)
+    if coolship.x>WIDTH:
+        coolship.x=+0
+    if coolship.x<0:
+        coolship.x=WIDTH
+    if keyboard.left:
+        coolship.x=coolship.x-7
+    if keyboard.right:
+        coolship.x=coolship.x+7
+    
+def on_key_down(key):
+    if key==keys.UP:
+        bullet=Actor("bullet")
+        bullet.pos=coolship.pos
+        ammo.append(bullet) 
+    
+
+
+
  
 
 pgzrun.go()
